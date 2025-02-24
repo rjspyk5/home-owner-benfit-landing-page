7bab512834add9d8d78ca85f59488efee46224d3
rjspyk5 <rjspyk5@gmail.com>
2025-02-24 11:19:38 +0600

    setup main background and project stucture

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -4,10 +4,16 @@ import { Footer } from '../Pages/Shared/Footer/Footer'
 
 export const RootLayout = () => {
   return (
-    <div>
+    <div className='min-h-screen relative'>
+      <div className='fixed inset-0 bg-[url("https://homeownersbenefit.nebs-it.com/wp-content/uploads/2024/05/solar-panels-roof-solar-cell-Copy-scaled.jpg")] bg-cover bg-center bg-no-repeat -z-20'></div>
+      <div className="fixed inset-0 bg-black/65 -z-10"></div>
+      <div className='relative z-10 flex flex-col min-h-screen'>
         <Navbar/>
-        <Outlet/>
+        <div className='flex-grow'>
+          <Outlet/>
+        </div>
         <Footer/>
+      </div>
     </div>
   )
 }
