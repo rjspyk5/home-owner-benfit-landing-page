563b114128aa686ed507d1e65785b575ed175651
rjspyk5 <rjspyk5@gmail.com>
2025-02-25 01:04:47 +0600

    left side complete

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -1,19 +1,19 @@
-import { Navbar } from '../Pages/Shared/Navbar/Navbar'
-import { Outlet } from 'react-router'
-import { Footer } from '../Pages/Shared/Footer/Footer'
+import { Navbar } from "../Pages/Shared/Navbar/Navbar";
+import { Outlet } from "react-router";
+import { Footer } from "../Pages/Shared/Footer/Footer";
 
 export const RootLayout = () => {
   return (
-    <div className='min-h-screen relative'>
+    <div className="min-h-screen relative">
       <div className='fixed inset-0 bg-[url("https://homeownersbenefit.nebs-it.com/wp-content/uploads/2024/05/solar-panels-roof-solar-cell-Copy-scaled.jpg")] bg-cover bg-center bg-no-repeat -z-20'></div>
       <div className="fixed inset-0 bg-black/65 -z-10"></div>
-      <div className='relative z-10 flex flex-col min-h-screen  lg:max-w-[1280px] mx-auto p-3'>
-        <Navbar/>
-        <div className='flex-grow'>
-          <Outlet/>
-        </div>
-        <Footer/>
+      <div className="relative flex flex-col z-10 lg:max-w-[1280px] mx-auto p-3">
+        <Navbar />
+       <div className="flex-grow">
+       <Outlet />
+       </div>
+        <Footer />
       </div>
     </div>
-  )
-}
+  );
+};
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-
 export const Home = () => {
   return (
-    <div className='h-full flex flex-col md:flex-row items-center'>
-    <div className="w-[60%]">  <h1 className='text-6xl font-semibold leading-[6rem]'>Power Your Home. Empower Your Future. Switch to Solar Today!</h1></div>
-      <div className=" w-[40%]"></div>
-      
+    <div className="flex min-h-[calc(100vh-130px)] flex-col md:flex-row items-center">
+      <div className="md:w-[60%]">
+        <h1 className="md:text-6xl text-[28px] font-semibold md:leading-[5rem] text-white mb-0">
+          Power Your Home. Empower Your Future. Switch to Solar Today!
+        </h1>
+        <ul className="text-[13px] md:text-base list-disc text-white space-y-1 pl-5">
+          <li className="pl-1">Experience Lower Electricity Bills with Solar Power</li>
+          <li className="pl-1">Clean Energy Independence. Invest in a Greener Future</li>
+          <li className="pl-1">Transform Your Roof into a Revenue Stream. Monetize Surplus Solar Energy Through Grid Export</li>
+        </ul>
+      </div>
+      <div className=" md:w-[40%]">
+        <div className="p-4 md:p-7 rounded-2xl bg-white">
+          <h2 className="md:text-xl font-medium">Complete the steps to unlock your solar potential</h2>
+        </div>
 
+      </div>
     </div>
-  )
-}
+  );
+};
